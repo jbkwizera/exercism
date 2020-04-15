d2d54c84a8b990415fb7b98f462a400ea4833e1e
meraxex <jeanbaptistekwi@gmail.com>
2020-04-15 00:54:42 -0700

    Slow/exp. time solution --no error handling

--- python/change/change.py
+++ python/change/change.py
@@ -1,2 +1,16 @@
+import sys
+sys.setrecursionlimit(1500)
+
 def find_fewest_coins(coins, target):
-    return [target]
+    change = [1]*1000
+    if target == 0:
+        return []
+    elif target in coins:
+        return [target]
+    else:
+        for coin in [coin for coin in coins if coin <= target]:
+            change = min(
+                [change, [coin]+find_fewest_coins(coins, target-coin)],
+                key=lambda val: len(val)
+            )
+        return change
