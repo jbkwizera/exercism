8d67e8cf524bd11dfc255b32d80b577f8c35bd17
meraxex <jeanbaptistekwi@gmail.com>
2020-04-15 01:04:04 -0700

    Improved solution w/ memoization

--- python/change/change.py
+++ python/change/change.py
@@ -2,15 +2,23 @@ import sys
 sys.setrecursionlimit(1500)
 
 def find_fewest_coins(coins, target):
-    change = [1]*1000
+    memoize = dict()
     if target == 0:
         return []
-    elif target in coins:
+    return find_fewest_coins_helper(coins, target, memoize)
+
+def find_fewest_coins_helper(coins, target, memoize):
+    change = [1]*1000
+    if target in coins:
+        memoize[target] = [target]
         return [target]
+    elif target in memoize:
+        return memoize[target]
     else:
         for coin in [coin for coin in coins if coin <= target]:
             change = min(
-                [change, [coin]+find_fewest_coins(coins, target-coin)],
+                [change, [coin]+find_fewest_coins_helper(coins, target-coin, memoize)],
                 key=lambda val: len(val)
             )
+            memoize[target] = change
         return change
