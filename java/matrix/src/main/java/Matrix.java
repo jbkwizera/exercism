0785f5aa97c3f78bce45776528c30030ae24f1cd
meraxex <jeanbaptistekwi@gmail.com>
2020-04-23 21:34:50 -0700

    Matrix solution

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,15 +1,27 @@
-
+import java.util.Arrays;
 class Matrix {
+    private int[][] a;
 
     Matrix(String matrixAsString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] rows = matrixAsString.split("\\n+");
+        int ncols = rows[0].split("\\s+").length;
+        a = new int[rows.length][ncols];
+
+        for (int i = 0; i < rows.length; i++) {
+            String[] row = rows[i].split("\\s+");
+            for (int j = 0; j < row.length; j++)
+                a[i][j] = Integer.parseInt(row[j]);
+        }
     }
 
     int[] getRow(int rowNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return a[rowNumber-1];
     }
 
     int[] getColumn(int columnNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int[] col = new int[a.length];
+        for (int i = 0; i < a.length; i++)
+            col[i] = a[i][columnNumber-1];
+        return col;
     }
 }
