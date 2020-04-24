70368bcfe56d78e3eb34fedb9b2a654d935bdd8b
meraxex <jeanbaptistekwi@gmail.com>
2020-04-24 02:50:38 -0700

    HashSet solution

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,16 @@
+import java.util.HashSet;
 class IsogramChecker {
-
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        HashSet<Character> hs;
+        hs = new HashSet<Character>();
 
+        phrase = phrase.toLowerCase();
+        for (int i = 0; i < phrase.length(); i++) {
+            char c = phrase.charAt(i);
+            if (Character.isLetter(c) && hs.contains(c))
+                return false;
+            hs.add(c);
+        }
+        return true;
+    }
 }
