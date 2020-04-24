59a3bc212e812450970bf7fbcfdf3f6c7c07d94c
meraxex <jeanbaptistekwi@gmail.com>
2020-04-23 20:55:52 -0700

    ISBN Verifier solution

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,6 +1,20 @@
 class IsbnVerifier {
 
     boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        stringToVerify = stringToVerify.toLowerCase();
+        String isbn = "";
+        int sum = 0;
+        int i = 10;
+        for (char ch: stringToVerify.toCharArray()) {
+            if (Character.isDigit(ch) || ch == 'x') {
+                if (ch != 'x') sum += i*(ch - '0');
+                else           sum += 10;
+                i -= 1;
+                isbn += ch;
+            }
+            else if (ch != '-') return false;
+        }
+        if (isbn.length() != 10) return false;
+        return sum % 11 == 0;
     }
 }
