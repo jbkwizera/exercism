65d955ee8328b275f01466d285015083242531da
meraxex <jeanbaptistekwi@gmail.com>
2020-04-23 20:13:59 -0700

    Add ISBN-verifier

---
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -0,0 +1,6 @@
+class IsbnVerifier {
+
+    boolean isValid(String stringToVerify) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
