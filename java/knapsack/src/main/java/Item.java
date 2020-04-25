1834df771ab226bb540052bdc309e9165af21d12
meraxex <jeanbaptistekwi@gmail.com>
2020-04-25 01:51:31 -0700

    DP solution

---
+++ java/knapsack/src/main/java/Item.java
@@ -0,0 +1,6 @@
+public class Item {
+    public int weight;
+    public int value;
+    public Item(int w, int v)
+    {   this.weight = w; this.value = v; }
+}
--- java/knapsack/src/main/java/Knapsack.java
+++ java/knapsack/src/main/java/Knapsack.java
@@ -5,3 +5,18 @@ This is so that you get to practice creating classes and methods
 which is an important part of programming in Java.
 Please remove this comment when submitting your solution.
 */
+import java.util.ArrayList;
+public class Knapsack {
+
+    public int maximumValue(int capacity, ArrayList<Item> items) {
+        int[][] a = new int[items.size()+1][capacity+1];
+        for (int i = 1; i <= items.size(); i++) {
+            int vi = items.get(i-1).value;
+            int wi = items.get(i-1).weight;
+            for (int w = 0; w <= capacity; w++)
+                if (w < wi) a[i][w] = a[i-1][w];
+                else        a[i][w] = Math.max(a[i-1][w-wi]+vi, a[i-1][w]);
+        }
+        return a[items.size()][capacity];
+    }
+}
